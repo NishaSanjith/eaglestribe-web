f511900b2c04f506ba17a33ae4e5f18a72a202cb
Nisha A R <nisha@inspirehealthscience.com>
2025-06-16 21:20:58 +0400

    changed the site code

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Your Next.js config options go here.
-  // We can leave it empty for now since you are using local images.
-};
-
-// Use 'export default' for .mjs files instead of 'module.exports'
-export default nextConfig;
